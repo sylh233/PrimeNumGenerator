4084c973f40a3dd1258b61d7680ae1868b928ec4
Sylh <785168843@qq.com>
2025-08-29 21:02:09 +0800

    修改成只有没出现的合数才被打印出来

--- src/main.cpp
+++ src/main.cpp
@@ -12,15 +12,18 @@ int main()
     {
         arr_base[i-j]=i;
     }
-    
+
     int last = arr_base[top-1];
     for(int i = 2;i < top;i++)
     {
         int num = arr_base[i];
         for(int n = num,c = 2;n*c <= last&&n!=0;c++)
         {
-            arr_base[n*c] = 0;
-            cout << n << ":" << n*c <<endl;
+            if(arr_base[n*c] != 0)
+            {
+                arr_base[n*c] = 0;
+                cout << n << ":" << n*c <<endl;
+            }
         }
     }
     for(int i = 0,j=i;i < j+top;i++)
