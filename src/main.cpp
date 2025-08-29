dbbee8b3bd3e3a3e3d40ac9b67dceee1664e9042
Sylh <785168843@qq.com>
2025-08-29 17:07:16 +0800

    try how to use iof

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,23 @@
 
 int main()
 {
+    using namespace std;
+    
+    ifstream fin;
+    ofstream fout;
+    int top = 10;
+    fout.open("p.txt",ios::app);
+    for(int i =0;i<=top;i++)
+    {
+        fout<<i<<" ";
+    }
+    fout.close();
+    fin.open("p.txt");
+    int a;
+    while(fin >> a)
+    {
+        cout << a <<" ";
+    }
+    fin.close();
     return 0;
 }
